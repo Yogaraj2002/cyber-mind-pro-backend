0026e5b330841a90f77ce98fe2908a9f494acb9b
Yogaraj S <yogi81102@gmail.com>
2025-09-13 17:48:36 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,24 +1,17 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-
-dotenv.config();
-
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 5432,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false, // important for Render Postgres
+        rejectUnauthorized: false, 
       },
     },
-    logging: false,
+    logging: true, // temporarily enable logging
   }
 );
-
-export default sequelize;
